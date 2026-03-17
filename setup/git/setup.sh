7be123220acc782b5cb854f950a6601cdc5b973b
James Fosburgh <jwfosburgh@gmail.com>
2026-03-17 15:14:40 -0400

    git setup

--- setup/git/setup.sh
+++ setup/git/setup.sh
@@ -1,2 +1,7 @@
 git config --global user.email "jwfosburgh@gmail.com"
 git config --global user.name "James Fosburgh"
+
+ssh-keygen -t ed25519 -C "jwfosburgh@gmail.com"
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
